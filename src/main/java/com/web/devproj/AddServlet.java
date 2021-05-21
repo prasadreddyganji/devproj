e9a0c6716379c6c20d9127c6b2bbc763a3a76247
prasadreddyganji <prasadreddyganji461@gmail.com>
2021-05-21 09:31:20 +0530

    first

---
+++ src/main/java/com/web/devproj/AddServlet.java
@@ -0,0 +1,25 @@
+package com.web.devproj;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+public class AddServlet extends HttpServlet
+{
+  public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+  {
+	  int i = Integer.parseInt(req.getParameter("num1"));
+	  int j = Integer.parseInt(req.getParameter("num2"));
+	  
+	  int k =i+j; 
+	 	
+	  Cookie cookie =new Cookie("k", k+"");
+	  res.addCookie(cookie);
+	  res.sendRedirect("sq");
+  }
+}
---
+++ src/main/java/com/web/devproj/SqServlet.java
@@ -0,0 +1,28 @@
+package com.web.devproj;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SqServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		int k = 0;
+		Cookie cookies[] = req.getCookies();
+		
+		for(Cookie c:cookies)
+		{
+			if(c.getName().equals("k"))
+				k=Integer.parseInt(c.getValue());
+		}
+		k=k*k;
+		PrintWriter out = res.getWriter();
+		out.println("sq is: "+k);
+	}
+}
